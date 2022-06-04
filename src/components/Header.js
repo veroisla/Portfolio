6076031628ac4cac5d0ace82447436c72d59d94b
Veronica isla <veroislagleaniz@gmail.com>
2022-06-04 14:16:49 +0200

    started with header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,34 @@
 import '../styles/Core/Reset.scss';
 import '../styles/Core/Variables.scss';
 import '../styles/Components/Header.scss';
+import CV from '../CV.pdf';
+import ME from '../images/ME.jpg';
+import HeaderSocials from './HeaderSocials';
 
 function Header() {
-  return <p>Header</p>;
+  return (
+    <header className="header">
+      <h5>Hello I'm</h5>
+      <h1>Vero Isla</h1>
+      <h5 className="text-light">Front-end Developer</h5>
+      <div className="header__btn--div">
+        <button type="button" className="btn header__btn">
+          <a href={CV} download>
+            Download CV
+          </a>
+        </button>
+        <button type="button" className="btn header__btn">
+          <a href="#contact">¿Hablamos?</a>
+        </button>
+      </div>
+
+      <section className="header__socisls">
+        {' '}
+        <HeaderSocials />
+      </section>
+      <img className="header__img" src={ME} alt="Vero Isla"></img>
+      <a href="#contact">Scroll Down</a>
+    </header>
+  );
 }
 export default Header;
---
+++ src/components/HeaderSocials.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
+import '../styles/Components/Header.scss';
+
+const HeaderSocials = () => {
+  return (
+    <section className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/veronica-isla-garcia-de-leaniz/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {' '}
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/veroisla" target="_blank" rel="noreferrer">
+        <BsGithub />
+      </a>
+    </section>
+  );
+};
+
+export default HeaderSocials;
