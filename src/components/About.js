08500c9b079e7992d859fe6a7ed8517ac7c465a2
Veronica isla <veroislagleaniz@gmail.com>
2022-06-07 18:31:52 +0200

    section about me

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,21 @@ import '../styles/Core/Variables.scss';
 import '../styles/Components/About.scss';
 
 function About(params) {
-  return <section id="about">About</section>;
+  return (
+    <section id="about" className="about">
+      <h5 className="about__knowMe">Conóceme</h5>
+      <h2 className="about__aboutMe">Sobre mí</h2>
+      <p className="about__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus maiores
+        quis nam veniam repudiandae corrupti at, rem eaque optio ratione,
+        expedita reprehenderit commodi numquam possimus ducimus assumenda nihil
+        consequatur odio.
+      </p>
+      <div>
+        <div></div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
---
+++ src/components/Skills.js
@@ -0,0 +1,8 @@
+import '../styles/Core/Reset.scss';
+import '../styles/Core/Variables.scss';
+
+const Skills = () => {
+  return <section></section>;
+};
+
+export default Skills;
