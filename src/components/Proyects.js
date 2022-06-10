9ccf97b6ed29526b03b148f7c391221840bf0709
Veronica isla <veroislagleaniz@gmail.com>
2022-06-10 17:37:18 +0200

    added another proyect

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -8,6 +8,7 @@ import '../styles/Components/Proyects.scss';
 //ICONOS E IMÁGENES
 import { RiInformationLine } from 'react-icons/ri';
 import openSpaces from '../images/openSpaces.jpg';
+import LoveLace from '../images/loveLace.jpg';
 
 function Proyects(params) {
   return (
@@ -18,7 +19,7 @@ function Proyects(params) {
         <img
           className="proyect__img"
           src={openSpaces}
-          alt="Img Open Spaces"
+          alt="Img Proyect Open Spaces"
           title="Img Open Spaces"
         ></img>{' '}
         <div className="proyect__nameAndIcon">
@@ -29,6 +30,31 @@ function Proyects(params) {
           </Link> */}
           <a
             href="https://veroisla.github.io/Responsive-web/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BiLink className="proyect__icon" />
+          </a>
+        </div>
+      </article>
+
+      {/* Proyecto grupal presentacion */}
+
+      <article className="proyect__article">
+        <img
+          className="proyect__img"
+          src={LoveLace}
+          alt="Img Proyect LoveLace"
+          title="Img Open Spaces"
+        ></img>{' '}
+        <div className="proyect__nameAndIcon">
+          {' '}
+          <h4 className="proyect__name">Project #LoveLace</h4>
+          {/* <Link to="/OpenSpaces">
+            <RiInformationLine className="proyect__icon" />
+          </Link> */}
+          <a
+            href="http://beta.adalab.es/project-promo-p-module-1-team-5/"
             target="_blank"
             rel="noreferrer"
           >
