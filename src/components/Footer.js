268012ce32c4ca7624bc6e016258754ec4facd63
Veronica isla <veroislagleaniz@gmail.com>
2022-06-16 17:56:26 +0200

    added section footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import '../styles/Core/Reset.scss';
 import '../styles/Core/Variables.scss';
 import '../styles/Components/Footer.scss';
+import { BsArrowUpCircleFill } from 'react-icons/bs';
+import { GiIsland } from 'react-icons/gi';
 
 function Footer(params) {
-  return <p>Footer</p>;
+  return (
+    <footer className="footer">
+      <p className="footer__copyrigth">
+        © Verónica Isla <GiIsland className="footer__island" />
+      </p>
+      <a href="#home">
+        <BsArrowUpCircleFill className="footer__arrow" />
+      </a>{' '}
+    </footer>
+  );
 }
 
 export default Footer;
